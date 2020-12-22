c93835e4086f587ca767bd763b97e6466124232d
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 23:36:04 +0000

    First commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -22,25 +22,25 @@ def do_pack():
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
 
-    if path.exists(archive_path) is True:
-        try:
-            path_ls = archive_path.split("/")
-            file_name = path_ls[-1]
-            name_no_ext = file_name.split(".")[0]
-            p = "/data/web_static/releases/"
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}/'.format(p, name_no_ext))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, p, name_no_ext))
-            run('rm /tmp/{}'.format(file_name))
-            run('mv {}{}/web_static/* {}{}/'.format(p, name_no_ext,
-                                                    p, name_no_ext))
-            run('rm -rf {}{}/web_static'.format(p, name_no_ext))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(p, name_no_ext))
-            return True
-        except:
-            return False
-    return False
+    if path.exists(archive_path) is False:
+        return False
+    try:
+        path_ls = archive_path.split("/")
+        file_name = path_ls[-1]
+        name_no_ext = file_name.split(".")[0]
+        p = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(p, name_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, p, name_no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}{}/web_static/* {}{}/'.format(p, name_no_ext,
+                                                p, name_no_ext))
+        run('rm -rf {}{}/web_static'.format(p, name_no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(p, name_no_ext))
+        return True
+    except:
+        return False
 
 
 def deploy():
