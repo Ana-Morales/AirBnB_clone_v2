0347f244e61980a26fc21913c36a87a2d9472881
Ana-Morales <moralesanaruth@gmail.com>
2020-12-22 23:20:33 +0000

    Fix typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,7 +31,7 @@ def do_deploy(archive_path):
             file_name = path_ls[-1]
             name_no_ext = file_name.split(".")[0]
             p = "/data/web_static/releases/"
-            put(archive_path, "/tmp/")
+            put(archive_path, '/tmp/')
             run('mkdir -p {}{}'.format(p, name_no_ext))
             run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, p, name_no_ext))
             run('rm /tmp/{}'.format(file_name))
