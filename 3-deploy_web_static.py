9ecd11ef1e819b11beea20cf90be85c40e70f1f6
Ana-Morales <moralesanaruth@gmail.com>
2020-12-23 01:55:02 +0000

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -48,6 +48,6 @@ def do_deploy(archive_path):
 def deploy():
     """creates and distributes an archive to your web servers"""
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
     return do_deploy(archive_path)
